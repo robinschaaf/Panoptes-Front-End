8c32a1f7841604c6d12c2cfc435224165ab33e70
Sarah Allen <sarah@zooniverse.org>
2016-06-22 13:59:01 -0500

    Add dialog

---
+++ app/components/workflow-assignment-dialog.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Dialog from 'modal-form/dialog'
+
+export default WorkflowAssignmentDialog extends React.Component {
+  constructor(props) {
+    super(props);
+  },
+
+  statics: {
+    start(history, location, preferences) {
+      let WorkflowAssignmentDialog = this;
+      return Dialog.alert(<WorkflowAssignmentDialog />, {
+        className: 'workflow-assignment-dialog', 
+        closeButton: true,
+        onCancel: this.handleStayOnCurrentWorkflowAssignment.bind(null, location, preferences),
+        onSubmit: this.handleNewWorklfowAssignment.bind(null, history, location, preferences)
+      });
+    },
+
+    handleNewWorklfowAssignment(history, location, preferences) {
+      return history.replace({ pathname: location.pathname, search: `?workflow=${preferences.preferences.selected_workflow}` })
+    },
+
+    handleStayOnCurrentWorkflowAssignment(location, preferences) {
+      // Switch user back to workflow in query rather than nero assigned workflow
+      // TODO: Maybe if user selects after 3 prompts, opt out?
+      preferences.update({ "preferences.selected_workflow": location.query.workflow });
+      return preferences.save();
+    }
+  },
+
+  render() {
+    return (
+      <div className="content-container">
+        <p>You've done a great job classifying. We'd like to recommend a more advanced task workflow for you to try out.</p>
+        <p>You now have a setting in your profile if you change your mind want to change which workflow you are on later.</p>
+        <button type="submit">Try the new workflow</button>
+      </div>
+    )
+  }
+}
+
+
